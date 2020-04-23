fa8ab0227841ec5491f88f3a417ed5e21bd8ab9c
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2020-04-23 10:19:06 +0100

    [Service] Updated Example Function to Return Cakes

--- src/functionName.js
+++ src/functionName.js
@@ -1,13 +1,18 @@
 import { corsHeaders } from './configuration';
 
-export const handler = async (event, context) => {
-  console.log('I Ran!');
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(context));
+export const handler = async () => {
+  console.log('Handing Call to Get / ');
+
+  const cakes = [
+    { name: 'Strawberry Cake 🍓', sugar: 33, taste: 2 },
+    { name: 'Apple And Mint 🍏🍎', sugar: 23, taste: 1 },
+    { name: 'Meat Cake', sugar: 33, taste: 4 },
+    { name: 'Toffee And Chocolate Cake', sugar: 43, taste: 27 },
+  ];
 
   return {
     headers: corsHeaders,
-    body: JSON.stringify({ cake: 'good' }),
+    body: JSON.stringify(cakes),
   };
 };
 
