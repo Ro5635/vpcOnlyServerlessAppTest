028dd795097e50639cc1b1cc1dc50a1d0161dd38
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2020-04-23 15:47:20 +0100

    [Service] Added New Cakes to Cakes Selection

--- service/src/cakes.js
+++ service/src/cakes.js
@@ -6,8 +6,9 @@ export const handler = async () => {
   const cakes = [
     { name: 'Strawberry Cake 🍓', sugar: 33, taste: 2 },
     { name: 'Apple And Mint 🍏🍎', sugar: 23, taste: 1 },
-    { name: 'Meat Cake', sugar: 33, taste: 4 },
     { name: 'Toffee And Chocolate Cake', sugar: 43, taste: 27 },
+    { name: 'MarshMellow Melt Cake', sugar: 173, taste: 30 },
+    { name: 'Rainbow Cake', sugar: 17, taste: 28 },
   ];
 
   return {
